e3ee0439959f6f8889621959b608f7d5ea09b061
Pat <113007904+PattZac@users.noreply.github.com>
2022-12-04 19:33:24 +0100

    Add files via upload

---
+++ pppzZadatakCetri/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pppzZadatakCetri
+{
+    class KlasuX
+    {
+        private int broj;
+        public int Broj { get => broj; set => broj = value; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KlasuX x = new KlasuX();
+            x.Broj = 10;
+            Console.WriteLine(x.Broj);
+            Console.ReadKey();
+        }
+    }
+}
